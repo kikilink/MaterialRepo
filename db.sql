3be1ce08eb8b480dd1c952e5bd82262d7b38b03e
kikilinux <546324943@qq.com>
2018-04-07 14:23:05 +0800

    初始化数据库

---
+++ db.sql
@@ -0,0 +1,45 @@
+DROP DATABASE
+IF EXISTS newbeeDB;
+
+CREATE DATABASE newbeeDB DEFAULT CHARACTER
+SET utf8;
+
+CREATE TABLE category (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(64),
+	valid BOOLEAN,
+	creation date,
+	creator VARCHAR(32)
+) ENGINE = INNODB
+;
+
+CREATE TABLE type (
+	id INT AUTO_INCREMENT NOT NULL,
+	category_id INT NOT NULL,
+	name VARCHAR(64),
+	valid BOOLEAN,
+	creation date,
+	creator VARCHAR(32),
+	CONSTRAINT pk_id PRIMARY KEY(id, category_id),
+	CONSTRAINT fk_id FOREIGN KEY(category_id) REFERENCES category (id)
+) ENGINE = INNODB;
+
+CREATE TABLE materials (
+	id INT AUTO_INCREMENT NOT NULL,
+	type_id INT NOT NULL,
+	model VARCHAR(32) NOT NULL,
+	footprint VARCHAR(32),
+	vendor VARCHAR(32),
+	package VARCHAR(32),
+	modelType VARCHAR(32),
+	purchasing_date date;
+	storage INT NOT NULL,
+	unit_price DECIMAL,
+	delivery INT,
+	guide VARCHAR(2000),
+	remark VARCHAR(128),
+	CONSTRAINT pk_id PRIMARY KEY(id, type_id),
+	CONSTRAINT fk_id FOREIGN KEY(type_id) REFERENCES
+	type(id)
+	
+) ENGINE = INNODB
